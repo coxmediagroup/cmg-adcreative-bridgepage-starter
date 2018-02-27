58c2a8e6051888e24b9654ee0064d93b21fead09
Breece <r.breece@me.com>
2018-02-27 13:19:16 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// var plyr_options = { disableContextMenu : true, clickToPlay: true, controls: ['mute','volume','progress','play','fullscreen'] };
+// var modalPlayer = plyr.setup('#modalPlayer',plyr_options);
+
+
+
+$(document).ready(function () {
+
+    // var videos = document.querySelectorAll("#video");
+    // for (var i = 0; i < videos.length; i++) {
+    //     videos[i].addEventListener("click", function(e) {
+    //         track('Clicked ' + e.target.dataset.videoName + ' Video');
+    //         changeSource(e.target.dataset.videoId, e.target.dataset.videoName);
+    //         $('#myModal').modal('toggle');
+    //         setTimeout(function(){
+    //             modalPlayer[0].play();
+    //         }, 700)
+    //     });
+    // }
+    // $('#myModal').on('hide.bs.modal', function () {
+    //     modalPlayer[0].pause();
+    // });
+
+
+});
+
+
+
+// function changeSource(source, title) {
+//     modalPlayer[0].source({
+//         type:       'video',
+//         title:      title || 'example',
+//         sources: [{
+//             src:    source || 'jeZtoEXSGXQ',
+//             type:   'youtube'
+//         }]
+//     });
+// }
